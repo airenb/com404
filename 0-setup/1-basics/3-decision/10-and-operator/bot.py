0b72b5d2b38ead2a801f717201d99660f8f061f3
airenb <4busha58@solent.ac.uk>
2019-10-03 15:33:44 +0000

    Added code to demonstrate the use of a logical and operator.

---
+++ 0-setup/1-basics/3-decision/10-and-operator/bot.py
@@ -0,0 +1,12 @@
+#retrieve what was heard and seen 
+print("What did I hear?")
+heard = input()
+print("What did I see?")
+saw = input()
+
+if(heard =="grrr") and (saw == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+
+else:
+    print("I'm a little scared but I will continue.")
+    
