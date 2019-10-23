f1db8fb5fed93c4270a7e0d1d3d86a11e0dd895e
airenb <4busha58@solent.ac.uk>
2019-10-23 21:36:59 +0000

    added code to demonstrate the use of a user defined function with a parameter

---
+++ 0-setup/1-basics/5-functions/3-parameter/bot.py
@@ -0,0 +1,13 @@
+def escape_by(plan):
+    if(plan == "jumping over"):
+        print("we cannot escape that way! the boulder is too big! ")
+    elif(plan == "running around"):
+        print("we cannot escape that way! the boulder is moving too fast!")
+    elif(plan== "going deeper"):
+        print("that might just work! lets go deeper!")
+    else:
+        print("we cannot escape that way! the boulder is in the way!")
+
+    escape_by("jumping over")
+    escape_by("running around")
+    escape_by("going deeper")
