74cc616a7829f703cb150ae8721028f40184c664
airenb <4busha58@solent.ac.uk>
2019-10-24 13:59:42 +0000

    done

---
+++ AE1 Review- TCA 2/2-decision.py
@@ -0,0 +1,8 @@
+#message
+print( "Have you fear in your heart?")
+user_response = input()
+
+if(user_response== "yes"):
+    print("Fear is the path to the dark side. You cannot be a Jedi apprentice." )
+else:
+    print("The force is strong in you. You may be a Jedi apprentice.")
