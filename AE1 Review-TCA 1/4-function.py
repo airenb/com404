fd0b09837c800f79c9df75a6bfc7e7f5061238ac
airenb <4busha58@solent.ac.uk>
2019-10-24 13:39:28 +0000

    ?

---
+++ AE1 Review-TCA 1/4-function.py
@@ -0,0 +1,18 @@
+print("I wonder what is in my suitcase...")
+
+
+#defining function 
+def suitcase (item):
+    #calls
+    if (item("toothbrush")):
+        print("A toothbrush. Well, got to have clean teeth!")
+    elif(item("spidey suit")):
+        print("My Spidey suit! I won't be needing this. I am on holiday.")
+    else():
+        print("An unexpected item! It could be useful.")
+    
+print("")
+#calls for function 
+item("toothbrush")
+item("spidey suit")
+item()
