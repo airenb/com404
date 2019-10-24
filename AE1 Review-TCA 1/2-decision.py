0c58d3d1b3be955830fb4ccd7d5b8ee2768056c2
airenb <4busha58@solent.ac.uk>
2019-10-24 13:11:02 +0000

    done

---
+++ AE1 Review-TCA 1/2-decision.py
@@ -0,0 +1,10 @@
+#question
+print("Where is Forky?")
+user_response = input()
+#decisions 
+if (user_response == "with bonnie"):
+    print("Phew! Bonnie will be happy.")
+elif(user_response == "running away"):
+    print("Oh no! bonnie will be upset ")
+else:
+    print("Ah! i better look for him.")
