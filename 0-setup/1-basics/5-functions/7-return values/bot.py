bf9cb37cf2d72e4ce4de07f8ff05bce08b408f61
airenb <4busha58@solent.ac.uk>
2019-10-30 16:20:05 +0000

    complete later

---
+++ 0-setup/1-basics/5-functions/7-return values/bot.py
@@ -0,0 +1,9 @@
+def sum_weights(weight_1, weight_2):
+    print(" hoe much does each bot weigh?")
+    weight_1 = int(input())
+    weight_2 = int(input())
+    sum_weights = weight_1 + weight_2
+    return sum_weights
+
+
+
